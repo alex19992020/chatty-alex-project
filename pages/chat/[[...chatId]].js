060169a249f88b40c1e5a268992fff48356496b5
alex19992020 <alejandroantunez1999@gmail.com>
2023-06-26 18:34:38 -0700

    updated chat id

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the chat Id page</h1>
+    </div>
+  );
+}
