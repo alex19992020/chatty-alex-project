658ac794575769389a543b1361be6f26e2cf152e
alex19992020 <alejandroantunez1999@gmail.com>
2023-07-03 22:23:28 -0700

    set up auth0 and api routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <UserProvider>
       <Head>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div>
       <Head>
         <title>Next JS ChatGPT Starter</title>
       </Head>
       <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+      <div>
+        {!!user && <Link href="/api/auth/logout">Logout</Link>}
+        {!user && <Link href="/api/auth/login">Login</Link>}
+      </div>
     </div>
   );
 }
