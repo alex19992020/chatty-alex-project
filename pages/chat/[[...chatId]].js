203df2e1228d303f40bfc416c8a7bf1b952615f4
alex19992020 <alejandroantunez1999@gmail.com>
2023-07-05 17:32:49 -0700

    created footer and message form

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,7 +1,15 @@
 import { ChatSideBar } from "components/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("MESSAGE TEXT: ", messageText);
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +19,21 @@ export default function Home() {
         <ChatSideBar />
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="focus:outline-emerald w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,10 @@ export default function Home() {
           {!!user && <Link href="/api/auth/logout">Logout</Link>}
           {!user && (
             <>
-              <Link
-                href="/api/auth/login"
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-              >
+              <Link href="/api/auth/login" className="btn">
                 Login
               </Link>
-              <Link
-                href="/api/auth/signup"
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-              >
+              <Link href="/api/auth/signup" className="btn">
                 Signup
               </Link>
             </>
