aa9d09882a657bc56a85ad3c8bc956f64f5e3ad0
alex19992020 <alejandroantunez1999@gmail.com>
2023-07-11 20:19:37 -0700

    added chat history conversations

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -17,6 +17,7 @@ export default async function handler(req) {
     };
 
     let newChatId;
+    let chatMessages = [];
 
     if (chatId) {
       //add message to chat
@@ -35,6 +36,8 @@ export default async function handler(req) {
           }),
         }
       );
+      const json = await response.json();
+      chatMessages = json.chat.messages || [];
     } else {
       const response = await fetch(
         `${req.headers.get("origin")}/api/chat/createNewChat`,
@@ -52,7 +55,23 @@ export default async function handler(req) {
       const json = await response.json();
       chatId = json._id;
       newChatId = json._id;
+      chatMessages = json.messages || [];
     }
+
+    const messagesToInclude = [];
+    chatMessages.reverse();
+    let usedTokens = 0;
+    for (let chatMessage of chatMessages) {
+      const messageTokens = chatMessage.content.length / 4;
+      usedTokens = usedTokens + messageTokens;
+      if (usedTokens <= 2000) {
+        messagesToInclude.push(chatMessage);
+      } else {
+        break;
+      }
+    }
+
+    messagesToInclude.reverse();
 
     const stream = await OpenAIEdgeStream(
       "https://api.openai.com/v1/chat/completions",
@@ -64,7 +83,7 @@ export default async function handler(req) {
         method: "POST",
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          messages: [initialChatMessage, { content: message, role: "user" }],
+          messages: [initialChatMessage, ...messagesToInclude],
           stream: true,
         }),
       },
